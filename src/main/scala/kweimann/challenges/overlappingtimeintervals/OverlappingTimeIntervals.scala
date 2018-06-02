9f2bde4d57a3582666c4a9c7e5479599ae4db875
Kuba Weimann <kuba.weimann@gmail.com>
2018-06-02 10:21:51 +0200

    adds overlapping time intervals behaviour

---
+++ src/main/scala/kweimann/challenges/overlappingtimeintervals/OverlappingTimeIntervals.scala
@@ -0,0 +1,11 @@
+package kweimann.challenges.overlappingtimeintervals
+
+object OverlappingTimeIntervals {
+  import OverlappingTimeIntervals.Types._
+
+  object Types {
+    type Interval = (Int, Int)
+  }
+
+  def findUntimedIntervals(in: Seq[Interval]): List[Interval] = ???
+}
