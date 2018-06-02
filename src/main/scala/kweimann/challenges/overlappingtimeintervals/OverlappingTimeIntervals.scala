8956db8e70d34c8cb2903a205c361e44a4add529
Kuba Weimann <kuba.weimann@gmail.com>
2018-06-02 11:37:15 +0200

    adds overlapping time intervals implementation

--- src/main/scala/kweimann/challenges/overlappingtimeintervals/OverlappingTimeIntervals.scala
+++ src/main/scala/kweimann/challenges/overlappingtimeintervals/OverlappingTimeIntervals.scala
@@ -1,5 +1,7 @@
 package kweimann.challenges.overlappingtimeintervals
 
+import scala.collection.mutable
+
 object OverlappingTimeIntervals {
   import OverlappingTimeIntervals.Types._
 
@@ -7,5 +9,30 @@ object OverlappingTimeIntervals {
     type Interval = (Int, Int)
   }
 
-  def findUntimedIntervals(in: Seq[Interval]): List[Interval] = ???
+  def findUntimedIntervals(timeIntervals: List[Interval]): List[Interval] = {
+    // ordering by interval end
+    implicit val intervalOrdering: Ordering[Interval] = (a: Interval, b: Interval) => Ordering.Int.compare(a._2, b._2)
+
+    // needs at least 2 intervals to find any untimed intervals
+    if (timeIntervals.isEmpty || timeIntervals.tail.isEmpty) return List.empty
+
+    // the algorithm will iterate over an ordered list of time intervals
+    val heap = mutable.PriorityQueue(timeIntervals: _*)
+
+    // minStart is the smallest start of an untimed interval seen so far
+    var (minStart, _) = heap.dequeue()
+    var untimedIntervals = List.empty[Interval]
+
+    // dequeue all elements starting from the last interval ordered by interval end
+    while (heap.nonEmpty) {
+      val (start, end) = heap.dequeue()
+      // if next interval's end (note descending order) is smaller than the smallest start seen so far
+      // the space between these two points creates an untimed interval
+      if (end < minStart) untimedIntervals = (end, minStart) :: untimedIntervals
+      // update
+      minStart = Math.min(start, minStart)
+    }
+
+    untimedIntervals
+  }
 }
